5279f93a088b2bc7aabb7118d6dead3248eb5961
Julie Elie <julie.elie@gmail.com>
2016-12-08 15:59:24 -0800

    modified the code so it handles the case where there is more than a spike per time window

--- src/discrimination/gauss_filter_varying_window.m
+++ src/discrimination/gauss_filter_varying_window.m
@@ -37,17 +37,21 @@ for nt=1:size(spikes,1)
     for a = 1:length(index) % iterate through the number of bins that contain spikes
         ss=ss+1;
         currenttime=index(a);% this is the time bin where is the current spike
-        timeindex=find(timevec==index(a));% This is the position of the spike in timevec
-        futuretime=timevec(timeindex+1);% this is the arrival time of the following spike
-        pasttime=timevec(timeindex-1);% this is the arrival time of the preceding spike
-        after=futuretime-currenttime;% This is the time between the spike and the future spike
-        before=currenttime-pasttime;% This is the time between the spike and the preceeding spike
-        if futuretime==size(spikes,2)+1 % The current spike was the last spike
-            after=after*2+1; % More than double the time after the spike
-        elseif pasttime==0 % The current spike was the first spike of the trial
-            before=before*2+1; % more than double the time before the spike
+        if spikes(nt,currenttime)>1% there is more than a spike in this time window
+            Spikeh(ss) = 1;%This is the smallest distance we can handle? 1ms?
+        else
+            timeindex=find(timevec==index(a));% This is the position of the spike in timevec
+            futuretime=timevec(timeindex+1);% this is the arrival time of the following spike
+            pasttime=timevec(timeindex-1);% this is the arrival time of the preceding spike
+            after=futuretime-currenttime;% This is the time between the spike and the future spike
+            before=currenttime-pasttime;% This is the time between the spike and the preceeding spike
+            if futuretime==size(spikes,2)+1 % The current spike was the last spike
+                after=after*2+1; % More than double the time after the spike
+            elseif pasttime==0 % The current spike was the first spike of the trial
+                before=before*2+1; % more than double the time before the spike
+            end
+            Spikeh(ss)=min([before after]);% This is the distance in number of bins to the nearest neighbor spike
         end
-        Spikeh(ss)=min([before after]);% This is the distance in number of bins to the nearest neighbor spike
     end
 end
 
@@ -55,12 +59,13 @@ ntrials=size(spikes,1);
 temp=zeros(size(spikes));
 
 if Fig
-    figure(2)
+    F2=figure(2);
     for t=1:ntrials
         subplot(ntrials,1,t)
         cla
     end
 end
+Ymaxcc=nan(length(SpikeID),1);
 for a = 1:length(SpikeID);
     hwidth=Spikeh(a)*alpha_param; % we want the number of points of the gaussian to be proportional to alpha so that the distance to the nearest spike is always = 1sd
     tempwin=gausswin(hwidth*2+1, alpha_param)/sum(gausswin(hwidth*2+1, alpha_param));
@@ -80,50 +85,74 @@ for a = 1:length(SpikeID);
         endindex_temp = currenttime + hwidth;
         endindex_gauss = size(tempgauss,2);
     end
-    temp(trial,startindex_temp:endindex_temp)=temp(trial,startindex_temp:endindex_temp)+tempgauss(startindex_gauss:endindex_gauss);
+    temp(trial,startindex_temp:endindex_temp)=temp(trial,startindex_temp:endindex_temp) + spikes(trial,currenttime) * tempgauss(startindex_gauss:endindex_gauss);
     if Fig
         subplot(ntrials,1,trial)
         plot(startindex_temp:endindex_temp, tempgauss(startindex_gauss:endindex_gauss),'-r')
+        Ymaxcc(a) = max(tempgauss(startindex_gauss:endindex_gauss));
         hold on
-        plot([currenttime currenttime], [0 spikes(trial,currenttime)*0.1],'-k')
+        plot([currenttime currenttime], [0 spikes(trial,currenttime)*0.1],'-k','LineWidth',1.5)
         hold on
         xlim([0 size(spikes,2)+1]);
         
     end
 end
 if Fig
-    figure(2)
+    F2.Name='gaussian windows of all trials';
+    subplot(ntrials,1,ntrials)
     xlabel('Time in ms')
-    title('gaussian windows of all trials')
+%     Ymaxcc=nan(length(F2.Children),1);
+%     for cc=1:length(F2.Children)
+%         Ymaxcc(cc) = F2.Children(cc).YLim(2);
+%     end
+    YMAX=max(Ymaxcc);
+    YMAX=YMAX+YMAX/5;
+    if YMAX<0.2
+        YMAX=0.2;
+    end
+    for cc=1:length(F2.Children)
+        F2.Children(cc).YLim(2)=YMAX;
+    end
     hold off
 end
 spikesfiltered=temp; % This is of the same size as the original spikes input
+YMAX2 = max(max(temp));
+YMAX2=YMAX2+YMAX2/5;
 
 if Fig
-    figure(3)
+    F3=figure(3);
     for t=1:ntrials
         subplot(ntrials,1,t)
         cla
         Spikes_local = find(spikes(t,:));
         for ss=1:length(Spikes_local)
-            plot([Spikes_local(ss) Spikes_local(ss)],[0 0.25],'-k');
+            plot([Spikes_local(ss) Spikes_local(ss)],[0 YMAX2/5],'-k','LineWidth',1.5);
             hold on
         end
-        plot((1:size(spikes,2)), spikesfiltered(t,:),'-r')
-        title(sprintf('Gaussian filtered spike train %d, Alpha=%d',t, alpha_param));
-        xlabel('Time in ms')
-        ylabel('spike rate spike/ms')
+        plot((1:size(spikes,2)), spikesfiltered(t,:),'-r', 'LineWidth',1.5)
         xlim([0 size(spikes,2)+1])
+        ylim([0 YMAX2])
     end
+    F3.Name=sprintf('Gaussian filtered spike train Alpha=%d', alpha_param);
+    subplot(ntrials,1,1)
+    ylabel('spike rate spike/ms')
+    subplot(ntrials,1,ntrials)
+    xlabel('Time (ms)')
     hold off
     figure(4)
     cla
-    [AX,~]=plotyy(1:size(spikes,2),mean(spikes,1),1:size(spikes,2),mean(spikesfiltered,1));
+    [AX,H1,H2]=plotyy(1:size(spikes,2),mean(spikes,1),1:size(spikes,2),mean(spikesfiltered,1));
     ylabel(AX(1),'Average Spike Count')
     ylabel(AX(2), 'Filtered Spike rate')
     xlabel('Time in ms')
     xlim(AX(1),[0 size(spikes,2)+1])
     xlim(AX(2),[0 size(spikes,2)+1])
+    ylim(AX(1),[0 YMAX2])
+    ylim(AX(2),[0 YMAX2])
+    set(H1, 'LineWidth', 1.5)
+    set(H2, 'LineWidth', 1.5)
+    set(AX(1), 'LineWidth', 1.5)
+    set(AX(2), 'LineWidth', 1.5)
     title(sprintf('average result of the gaussian filtering over trials with alpha=%d',alpha_param));
 end
 
